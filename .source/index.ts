4d84b467a1b7699628bd4c3af4c3573277555858
mrpitch <mrpitch@outlook.com>
2025-08-04 16:58:57 +0200

    refactor(fumadocs): update global styles and import paths
    - Removed deprecated global CSS imports from `layout.tsx` and replaced them with a new import from `@/lib/styles/global.css`.
    - Updated `tsconfig.json` to include path mapping for `@/lib/*`, improving module resolution.
    - Deleted unused files: `.source/index.ts` and `global.css`, streamlining the project structure.

--- .source/index.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck -- skip type checking
-import * as docs_0 from "../src/content/docs/index.mdx?collection=docs&hash=1753193124248"
-import { _runtime } from "fumadocs-mdx"
-import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/magnus/Dev/payloadcms-blank/src/content/docs/index.mdx"}, data: docs_0 }], [])
