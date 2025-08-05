50309b2ded5c5eecc11a64d850930ca4196cddcb
mrpitch <mrpitch@outlook.com>
2025-08-05 19:33:31 +0200

    refactor(layout): enhance documentation layout structure
    - Updated `layout.tsx` to define a static `pageTree` structure for documentation navigation, improving clarity and maintainability.
    - Removed commented-out code related to dynamic sidebar navigation, streamlining the component.
    - Adjusted sidebar navigation items to reflect new documentation URLs, ensuring accurate routing.
    - Enhanced the organization of documentation components for better user experience.

--- src/app/(fumadocs)/docs/layout.tsx
+++ src/app/(fumadocs)/docs/layout.tsx
@@ -4,14 +4,81 @@ import { baseOptions } from '@/fumadocs/layout.config'
 import { getMenuBySlug } from '@/lib/utils/getCollection'
 import type { Menu } from '@payload-types'
 import { createPageTree } from '@/payload/utils/create-page-tree'
+import type { PageTree } from 'fumadocs-core/server'
 
-const sidebarNavigation = (await getMenuBySlug('topic-1')) as Menu
-console.log('sidebarNavigation', JSON.stringify(sidebarNavigation, null, 2))
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: ReactNode
+  params: { slug: string }
+}) {
+  //const { slug } = await params
+  //const menuSlug = 'topic-2'
+  //const sidebarNavigation = (await getMenuBySlug(menuSlug)) as Menu
 
-const pageTree = createPageTree(sidebarNavigation)
-console.log('pageTree', JSON.stringify(pageTree, null, 2))
-
-export default function Layout({ children }: { children: ReactNode }) {
+  //const pageTree = createPageTree(sidebarNavigation)
+  //console.log('pageTree', JSON.stringify(pageTree, null, 2))
+  const pageTree = {
+    name: 'Documentation',
+    children: [
+      {
+        type: 'folder' as const,
+        root: true,
+        index: {
+          type: 'page' as const,
+          name: 'Doc 1',
+          url: '/docs/doc-1',
+        },
+        name: 'Components',
+        children: [
+          {
+            type: 'page' as const,
+            name: 'Doc 1',
+            url: '/docs/doc-1',
+          },
+          {
+            type: 'page' as const,
+            name: 'Doc 2',
+            url: '/docs/doc-2',
+          },
+          {
+            type: 'folder' as const,
+            name: 'SubTopic',
+            children: [
+              {
+                type: 'page' as const,
+                name: 'Doc 3',
+                url: '/docs/doc-3',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: 'folder' as const,
+        name: 'Getting Started',
+        root: true,
+        index: {
+          type: 'page' as const,
+          name: 'Doc 5',
+          url: '/docs/doc-5',
+        },
+        children: [
+          {
+            type: 'page' as const,
+            name: 'Doc 5',
+            url: '/docs/doc-5',
+          },
+          {
+            type: 'page' as const,
+            name: 'Doc 6',
+            url: '/docs/doc-6',
+          },
+        ],
+      },
+    ],
+  }
   return (
     <DocsLayout
       sidebar={{
@@ -20,12 +87,14 @@ export default function Layout({ children }: { children: ReactNode }) {
           {
             title: 'Components 1',
             description: 'Hello World!',
-            url: '/docs/page-1',
+            url: '/docs/doc-1',
+            urls: new Set(['/docs/doc-1', '/docs/doc-2', '/docs/doc-3']),
           },
           {
             title: 'Components 2',
             description: 'Hello World!',
-            url: '/docs/components',
+            url: '/docs/doc-5',
+            urls: new Set(['/docs/doc-5', '/docs/doc-6']),
           },
         ],
       }}
